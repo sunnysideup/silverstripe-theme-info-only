c22c4dda26214472a4f1aacb253234c4795f37c1
Sion <sion98madden@gmail.com>
2023-02-10 17:40:46 +1300

    WIP

--- src/js/features/fieldsToc.js
+++ src/js/features/fieldsToc.js
@@ -4,7 +4,7 @@
  * It will look for all data-field and create a Fields Table of Contents
  *
  */
-export function fieldsTocInit(onFirstInit) {
+export function fieldsTocInit(onFirstInit, currentSelection) {
     const fieldsToc = document.getElementById('fieldsToc')
     if (fieldsToc) {
         const selectors = document.querySelectorAll('[data-field]')
@@ -18,10 +18,11 @@ export function fieldsTocInit(onFirstInit) {
             }
             // console.log(value)
         })
-        var currentSelection = []
 
         const ulEl = document.createElement('ul')
         ulEl.id = 'filterUl'
+
+        const els = []
 
         // for each of the items...
         items.forEach(value => {
@@ -29,13 +30,29 @@ export function fieldsTocInit(onFirstInit) {
             const aEl = document.createElement('a')
             aEl.innerHTML = value
 
-            aEl.arrayOfSelectors = arrayOfSelectors;
-            aEl.currentSelection = currentSelection;
             // on click
-            aEl.addEventListener('click', aElEventHandler)
+            aEl.addEventListener('click', function (event) {
+                const test = this.innerHTML
+                this.classList.toggle('current')
+                this.classList.toggle('link')
+                const pos = currentSelection.indexOf(test)
+                if (this.classList.contains('current')) {
+                    if (pos === -1) {
+                        currentSelection.push(value)
+                    }
+                } else {
+                    if (pos !== -1) {
+                        currentSelection.splice(pos, 1)
+                    }
+                }
+                event.preventDefault()
+                filterIterate(arrayOfSelectors, currentSelection);
+                return false
+            })
 
             aEl.href = '#'
             aEl.classList.add('link')
+            els.push(aEl);
 
             // append
             listEl.appendChild(aEl)
@@ -44,29 +61,19 @@ export function fieldsTocInit(onFirstInit) {
         })
         if (!onFirstInit) {
             document.getElementById('filterUl').remove()
+            filterIterate(arrayOfSelectors, currentSelection)
+            els.forEach(aEl => {
+                if (currentSelection.includes(aEl.innerHTML)) {
+                    aEl.classList.toggle('current')
+                    aEl.classList.toggle('link')
+                }
+            })
         }
         fieldsToc.appendChild(ulEl)
     }
 }
 
-function aElEventHandler(event) {
-    const arrayOfSelectors = event.currentTarget.arrayOfSelectors
-    const currentSelection = event.currentTarget.currentSelection
-    const value = event.currentTarget.innerHTML
-    const test = this.innerHTML
-    this.classList.toggle('current')
-    this.classList.toggle('link')
-    const pos = currentSelection.indexOf(test)
-    if (this.classList.contains('current')) {
-        if (pos === -1) {
-            currentSelection.push(value)
-        }
-    } else {
-        if (pos !== -1) {
-            currentSelection.splice(pos, 1)
-        }
-    }
-    event.preventDefault()
+function filterIterate(arrayOfSelectors, currentSelection) {
     arrayOfSelectors.forEach(div => {
         const value = div.getAttribute('data-field').trim()
         const empty = currentSelection.length === 0
@@ -77,5 +84,4 @@ function aElEventHandler(event) {
             div.style.display = 'none'
         }
     })
-    return false
 }
--- src/js/features/initFeatures.js
+++ src/js/features/initFeatures.js
@@ -2,10 +2,12 @@ import {addLinkInit} from './addLink';
 import {showHideCurrentSitesInit} from './ShowHideCurrentSites';
 import {fieldsTocInit} from './fieldsToc';
 
+let currentFieldsFilters = [];
+
 siteInit(true);
 
 export function siteInit(firstInit=false) {
     addLinkInit();
     showHideCurrentSitesInit(firstInit);
-    fieldsTocInit(firstInit);
+    fieldsTocInit(firstInit, currentFieldsFilters);
 }
